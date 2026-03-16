de12c0b364f04ffc50d77af34617ced296de7d4d
Viacheslav Epifanov <vyache.epif@gmail.com>
2026-03-16 15:10:15 +0100

    refactor: change default (empty) Cell definition

--- src/main/java/com/jetbrains/vyache/task/Cell.java
+++ src/main/java/com/jetbrains/vyache/task/Cell.java
@@ -2,5 +2,9 @@ package com.jetbrains.vyache.task;
 
 public record Cell(char character, CellAttributes attributes) {
 
-    public static final Cell EMPTY = new Cell(' ', CellAttributes.DEFAULT);
+    public static final Cell EMPTY = new Cell(Character.MIN_VALUE, CellAttributes.DEFAULT);
+
+    public boolean isEmpty() {
+        return character == Character.MIN_VALUE;
+    }
 }
