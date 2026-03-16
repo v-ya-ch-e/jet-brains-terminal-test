4920f8dc2bf15608dba0b155d8a318b085cb0258
Viacheslav Epifanov <vyache.epif@gmail.com>
2026-03-16 13:29:46 +0100

    feat: cursor functionality encapsulation and implementation
    Add class Cursor with its encapsulated functionality (moving, initialisation, edge controlling, current attributes configuration)

---
+++ src/main/java/com/jetbrains/vyache/task/Cursor.java
@@ -0,0 +1,95 @@
+package com.jetbrains.vyache.task;
+
+public class Cursor {
+
+    private final TerminalBuffer terminalBuffer;
+    private int row;
+    private int col;
+    private CellAttributes currentAttributes;
+
+    // Initialization
+
+    public Cursor(TerminalBuffer terminalBuffer, int row, int col, CellAttributes currentAttributes) {
+        this.terminalBuffer = terminalBuffer;
+        this.row = Math.max(0, Math.min(row, terminalBuffer.getHeight() - 1));
+        this.col = Math.max(0, Math.min(col, terminalBuffer.getWidth() - 1));
+        this.currentAttributes = currentAttributes;
+    }
+
+    public Cursor(TerminalBuffer terminalBuffer, int row, int col) {
+        this(terminalBuffer, row, col, CellAttributes.DEFAULT);
+    }
+
+    public Cursor(TerminalBuffer terminalBuffer) {
+        this(terminalBuffer, 0, 0, CellAttributes.DEFAULT);
+    }
+
+    // Setting the cursor position
+
+    public void setPosition(int row, int col) {
+        this.row = Math.max(0, Math.min(row, terminalBuffer.getHeight() - 1));
+        this.col = Math.max(0, Math.min(col, terminalBuffer.getWidth() - 1));
+    }
+
+    public void setRow(int row) {
+        this.row = Math.max(0, Math.min(row, terminalBuffer.getHeight() - 1));
+    }
+
+    public void setCol(int col) {
+        this.col = Math.max(0, Math.min(col, terminalBuffer.getWidth() - 1));
+    }
+
+    // Getting the cursor position
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    // Moving the cursor
+
+    public void moveUp(int n) {
+        row = Math.max(0, row - n);
+    }
+
+    public void moveDown(int n) {
+        row = Math.min(terminalBuffer.getHeight() - 1, row + n);
+    }
+
+    public void moveLeft(int n) {
+        col = Math.max(0, col - n);
+    }
+
+    public void moveRight(int n) {
+        col = Math.min(terminalBuffer.getWidth() - 1, col + n);
+    }
+
+    // Attribute management
+
+    public void setCurrentAttributes(CellAttributes attributes) {
+        this.currentAttributes = attributes;
+    }
+
+    public void resetCurrentAttributes() {
+        this.currentAttributes = CellAttributes.DEFAULT;
+    }
+
+    public CellAttributes getCurrentAttributes() {
+        return currentAttributes;
+    }
+
+    public void setCurrentForeground(TerminalColor color) {
+        this.currentAttributes = this.currentAttributes.withForeground(color);
+    }
+
+    public void setCurrentBackground(TerminalColor color) {
+        this.currentAttributes = this.currentAttributes.withBackground(color);
+    }
+
+    public void setCurrentStyles(StyleFlag... flags) {
+        this.currentAttributes = this.currentAttributes.withStyles(flags);
+    }
+}
